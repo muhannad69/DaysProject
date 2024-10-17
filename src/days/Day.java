3c4d9f832821fbb8a243a1a074914a39fc41ebcd
muhannad69 <ekasheh@sheridancollege.ca>
2024-10-17 18:13:02 -0400

    Modified code with enums to print all days

---
+++ src/days/Day.java
@@ -0,0 +1,5 @@
+package days;
+
+public enum Day {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+}
--- src/days/Days.java
+++ src/days/Days.java
@@ -1,36 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-// Added a new comment
 package days;
 
 import java.util.Scanner;
 
-/**This class takes String input from user 
- * and calls method  to print the 
- * name of the day.
- * if the input is not given in String, 
- * program will accept and produce wrong results. 
- * change the code to use enums which avoids String input 
- * then print the week day names. 
- *
- * @author sivagamasrinivasan
- jn 22nd
- */
 public class Days {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-         
-    Scanner in =new Scanner(System.in);
-    System.out.println("Enter the day number (one,two,three,four,five) in string");
-    String code = in.next();
-    Weekdays t= new Weekdays();
-    t.nameOfDay(code); 
-    }// TODO code application logic here
-    
-    
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the day number (1 for MONDAY, 2 for TUESDAY, etc.):");
+        int dayNumber = in.nextInt();
+
+        Day selectedDay = Day.values()[dayNumber - 1];  // Access the correct enum value
+
+        Weekdays t = new Weekdays();
+        t.printDay(selectedDay);  // Call method to print the selected day
+
+        System.out.println("All days of the week:");
+        for (Day day : Day.values()) {
+            System.out.println(day);  // Print all days using the enum
+        }
+    }
 }
